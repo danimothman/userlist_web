4e5d7708fc95a715da187e5e708acd8814094eae
Your Name <you@example.com>
2020-06-17 17:39:17 +0900

    4th Commit

--- app.js
+++ app.js
@@ -2,23 +2,20 @@ var express = require('express')
 var path = require('path')
 var mysql = require('mysql')
 var app = express()
+var apiRouter = require('./routes/api_routes')
 
-sql_config = {
-    host:'localhost',
-    user:'root',
-    password:'1234',
-    database:'o2'
-}
 
 var db = mysql.createConnection(sql_config)
 
 
-db.connect
 app.set('views', path.resolve(__dirname + '/views'))
 app.set('view engine','ejs')
 
+
 app.use(express.urlencoded())
 app.use(express.json())
+
+app.use('/', apiRouter)
 
 app.get('/hello',(request , response)=>{
 
